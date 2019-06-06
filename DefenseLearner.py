52e74e6418d7b470aa9174d879b47cc954f76708
Taha Eghtesad <tahaeghtesad@gmail.com>
2019-06-06 14:13:33 -0500

    Added history attributes for defender.

--- DefenseLearner.py
+++ DefenseLearner.py
@@ -24,21 +24,30 @@ class DefenseLearner(BaseDefender):
         self.experience = experience
         self.logger = logging.getLogger(__name__)
 
+        self.last_probe = [-1] * self.m
+        self.last_reimage = [0] * self.m
+
     def update_utility(self, u):
         super().update_utility(u)
         self.experience.record_reward(u)
 
     def select_action(self, time, last_probe):
 
+        if last_probe != -1:
+            self.last_probe[last_probe] = time
+
         new_state = []
-        for server in self.servers:
+        for i, server in enumerate(self.servers):
             up = int(server['status'] == -1)
             time_to_up = 0 if up else server['status'] + self.downtime - time
             observed_progress = server['progress']
             prob = (1 - math.exp(-self.alpha * (server['progress'] + 1)))  # probability of attacker controlling that server
 
+            time_since_last_probe = time - self.last_probe[i] if self.last_probe[i] != -1 else -10  # Why -10?
+            time_since_last_reimage = time - self.last_reimage[i]
+
             new_state.append(
-                [up, time_to_up, observed_progress]
+                [up, time_to_up, observed_progress, time_since_last_probe, time_since_last_reimage]
             )
 
         self.experience.record_state(new_state)
@@ -55,6 +64,10 @@ class DefenseLearner(BaseDefender):
 
         if self.train:
             self.experience.train_model()
+
+        if action != 0:
+            self.last_reimage[action - 1] = time
+            self.last_probe[action - 1] = -1
 
         return action - 1
 
