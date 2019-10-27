abcbdc3668c903a2e4ac3eca82495a6326c7d3f9
Taha Eghtesad <tahaeghtesad@gmail.com>
2019-10-26 19:00:23 -0500

    Update manager

--- manager.py
+++ manager.py
@@ -8,12 +8,12 @@ from util.AttackerProcessor import AttackerProcessor
 from util.DefenderProcessor import DefenderProcessor
 import matplotlib.pyplot as plt
 
-steps = 5000
-episodes = 10
+steps = 1000
+episodes = 1
 m = 10
 
 
-def add_plots(attacker, defender, which):
+def add_plots(attacker, defender, which, smoothing_weight=.9):
     env = gym.make('MTD-v0', m=m, time_limit=steps, utenv=0, setting=1, ca=0.2)
 
     ap = AttackerProcessor(m=m)
@@ -62,7 +62,7 @@ def add_plots(attacker, defender, which):
         smoothed = []
         smooth = 0
         for i in range(len(aul)):
-            smooth = .999 * smooth + .001 * aul[i][1]
+            smooth = smoothing_weight * smooth + (1-smoothing_weight) * aul[i][1]
             smoothed.append(smooth)
 
         plt.plot([c[0] for c in aul], smoothed, label=f'{attacker.__class__.__name__} Smoothed Reward')
@@ -70,13 +70,14 @@ def add_plots(attacker, defender, which):
     else:
         plt.plot([0, steps * episodes], [du / steps / episodes, du / steps / episodes], alpha=0.5,
                  label=f'{defender.__class__.__name__} Average Reward')
-        rewards = sorted(random.sample(dul, 500), key=lambda i: i[0])
+        # rewards = sorted(random.sample(dul, 500), key=lambda i: i[0])
+        rewards = dul
         plt.plot([c[0] for c in rewards], [c[1] for c in rewards], alpha=0.3, linestyle='-', label=f'{defender.__class__.__name__} Reward')
 
         smoothed = []
         smooth = 0
         for i in range(len(dul)):
-            smooth = .999 * smooth + .001 * dul[i][1]
+            smooth = smoothing_weight * smooth + (1-smoothing_weight) * dul[i][1]
             smoothed.append(smooth)
 
         plt.plot([c[0] for c in dul], smoothed, label=f'{defender.__class__.__name__} Smoothed Reward')
@@ -89,9 +90,9 @@ def add_plots(attacker, defender, which):
 
 
 add_plots(UniformAttacker(), UniformDefender(), 0)
-add_plots(UniformAttacker(), DQNWrapper.load('weights/defender_7d1cc244_weights.pkl'), 0)
-# add_plots(DQNWrapper.load('weights/attacker_63be0de9_weights.pkl'), UniformDefender(), 1)
-# add_plots(UniformAttacker(), UniformDefender(), 0)
+# add_plots(UniformAttacker(), DQNWrapper.load('weights/defender_7d1cc244_weights.pkl'), 0)
+# add_plots(DQNWrapper.load('weights/attacker_63be0de9_weights_good.pkl'), UniformDefender(), 1)
+# add_plots(UniformAttacker(), UniformDefender(), 1)
 
 plt.savefig('defender_reward_plot.png', dpi=800)
 # plt.show()
