362b4ccada8d4f91eb3f2bd2c64858311aa30a6f
Taha Eghtesad <tahaeghtesad@gmail.com>
2019-11-12 13:05:26 -0600

    Get job_id in run.py

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 import subprocess
+from subprocess import PIPE
 
 
 def run(params):
     player, episodes, opponent, ef, ev, layers, gamma, dueling, double, prioritized_replay, normalization = params
     print(f"Running... {['sbatch', 'run.srun.sh', player, episodes, opponent, ef, ev, layers, gamma, dueling, double, prioritized_replay, normalization]}")
-    # completed = \
-    subprocess.run(['sbatch', 'run.srun.sh', player, episodes, opponent, ef, ev, layers, gamma, dueling, double, prioritized_replay, normalization])
-    # job_id = int(completed.stdout.decode("ascii").split('\n')[1].split(' ')[-1])
-    # return job_id
+    completed = subprocess.run(['sbatch', 'run.srun.sh', player, episodes, opponent, ef, ev, layers, gamma, dueling, double, prioritized_replay, normalization], stdout=PIPE)
+    job_id = int(completed.stdout.decode("ascii").split('\n')[1].split(' ')[-1])
+    return job_id
 
 
-episodes = ['250']
-efs = ['0.2', '0.5']  # 0.2
-evs = ['0.02', '0.1']  # 0.02
-layers = ['x', '64', '64,64']  # 25, 25
-gammas = ['0.99', '0.97']  # 0.99
+episodes = ['250', '500']
+efs = ['0.2']  # 0.2
+evs = ['0.02']  # 0.02
+layers = ['64', '64,64']  # 25, 25
+gammas = ['0.99']  # 0.99
 duelings = ['True', 'False']
 doubles = ['True', 'False']
-prioritized_replays = ['True', 'False']
-normalizations = ['True', 'False']
+prioritized_replays = ['False']
+normalizations = ['True']
 
 attacker_opponents = ['PCPDefender']
 defender_opponents = ['UniformAttacker']
@@ -35,13 +35,12 @@ for ep in episodes:
                                 for normalization in normalizations:
                                     for opponent in attacker_opponents:
                                         runs.append(('attacker', ep, opponent, ef, ev, layer, gamma, dueling, double, prioritized_replay, normalization))
-                                    for opponent in defender_opponents:
-                                        runs.append(('defender', ep, opponent, ef, ev, layer, gamma, dueling, double, prioritized_replay, normalization))
+                                    # for opponent in defender_opponents:
+                                    #     runs.append(('defender', ep, opponent, ef, ev, layer, gamma, dueling, double, prioritized_replay, normalization))
 
 for r in runs:
-    # id = run(r)
-    # print(f'Job submitted with id: {id}')
-    run(r)
+    id = run(r)
+    print(f'Job submitted with id: {id}')
 
 # p = multiprocessing.Pool(int(multiprocessing.cpu_count()/2))
 #
